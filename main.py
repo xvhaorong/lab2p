aec20b9b36913233ef568eb4c9bb8c48f0fafaf9
xvhaorong <hxx5086@psu.edu>
2020-09-10 19:45:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+def getLetterGrade(gd):
+  if gd>=93.0:
+    lt="A"
+  elif gd>=90:
+    lt="A-"
+  elif gd>=87:
+    lt="B+"
+  elif gd>=83:
+    lt="B"
+  elif gd>=80:
+    lt="B-"
+  elif gd>=77:
+    lt="C+"
+  elif gd>=70:
+    lt="C"
+  elif gd>=60:
+    lt="D"
+  else:
+    lt="F"
+  return lt
+
+def run():
+  finalgrade=float(input("Enter your CMPSC 131 grade: "))
+  print(f"Your letter grade for CMPSC 131 is {getLetterGrade(finalgrade)}.")
+
+if __name__ == "__main__":
+  run()
+  
